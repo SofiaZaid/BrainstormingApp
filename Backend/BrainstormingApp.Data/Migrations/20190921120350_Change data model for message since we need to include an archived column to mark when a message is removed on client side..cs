aa2cfba135f7b5de32eaeee17548032f93541500
Sofia Zaid <sofiazaid@gmail.com>
2019-09-24 10:12:31 +0200

    Minor fix, UTC time format in data model.

---
+++ Backend/BrainstormingApp.Data/Migrations/20190921120350_Change data model for message since we need to include an archived column to mark when a message is removed on client side..cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BrainstormingApp.Data.Migrations
+{
+    public partial class Changedatamodelformessagesinceweneedtoincludeanarchivedcolumntomarkwhenamessageisremovedonclientside : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "MessageArchived",
+                table: "Messages",
+                nullable: true,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MessageArchived",
+                table: "Messages");
+        }
+    }
+}
---
+++ Backend/BrainstormingApp.Data/Migrations/20190921123506_changed to utc time format..cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BrainstormingApp.Data.Migrations
+{
+    public partial class changedtoutctimeformat : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "MessageCreated",
+                table: "Messages",
+                nullable: false,
+                defaultValueSql: "getutcdate()",
+                oldClrType: typeof(DateTime),
+                oldDefaultValueSql: "getdate()");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "MessageCreated",
+                table: "Messages",
+                nullable: false,
+                defaultValueSql: "getdate()",
+                oldClrType: typeof(DateTime),
+                oldDefaultValueSql: "getutcdate()");
+        }
+    }
+}
